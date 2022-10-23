5ecf09a37f907926c450879568e7727aa9905b94
Aditi-P22 <106650588+Aditi-P22@users.noreply.github.com>
2022-10-23 15:32:59 +0530

    Add files via upload
    made few changes in App.js and index.js files.

---
+++ App.js
@@ -0,0 +1,20 @@
+
+
+import Hero from "./components/hero/Hero";
+import Main from "./components/main/Main";
+import Navbar from "./components/navbar/Navbar";
+
+
+function App() {
+  return (
+     <>
+     <Navbar />
+     <Hero/>
+    <Main/>
+      </>
+  );
+  
+  
+}
+
+export default App;
